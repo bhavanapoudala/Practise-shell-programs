6fa427c2217d893bd4135be3410824d5833a9a4a
Bhavana <66380872+bhavanapoudala@users.noreply.github.com>
2021-02-26 23:47:30 +0530

    Update While.sh

--- repetition-statements/While.sh
+++ repetition-statements/While.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 echo "Entered the digit from command: " $1
 count=$1
-max=256
-while [ $count -le $max ]
+max=255
+while [ $result -le $max ]
 do
-	echo "2^" $count "=" $(($count*2))
-	((count++))
+	result=$(($count*2))
+	count=$(($count+1))
+	echo $result
 done
